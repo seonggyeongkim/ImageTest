329f02394ec1af8a1249131f74f4fc70801c4c27
seonggyeongkim <skim7902@gamil.com>
2016-09-27 20:22:03 +0900

    #3 Finished ImageTest

--- app/src/main/java/kr/hs/emirim/sbb9918/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sbb9918/imagetest/MainActivity.java
@@ -7,13 +7,52 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+     MyImage mImg;//myimage형인 변수 선언
+
+     static final int ORIGINAL=0;
+     static final int ROTATE=1; //변형 되는 값에 대한 상수
+     static final int TRANSLATE=2;
+     static final int SCALE=3;
+     static final int SKEW=4;
+     int choose=ORIGINAL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linear1=(LinearLayout)findViewById(R.id.linear1); //id를 가져 온다!
+        mImg=new MyImage(getApplicationContext()); //화면 (문맥) 객체 생성
+        linear1.addView(mImg); //MyImage추가!
     }
+
+    public void tranceformImage(View v){//매개 변수는 반드시 view로 한다! (버튼이 클릭 되면 호출 된다!)
+
+        switch (v.getId()){ //v.getId()가 클릭된 버튼의 배열을 가져다 준다!
+            case R.id.but_rotate:
+                choose=ROTATE;//상수 필드를 만들어 값 전달
+                break;
+            case R.id.but_translate:
+                choose=TRANSLATE;
+                break;
+            case R.id.but_scale:
+                choose=SCALE;
+                break;
+            case R.id.but_skew:
+                choose=SKEW;
+                break;
+        }
+
+        mImg.setChoose(choose);//MyImage.java에 choose값을 보내서 값 설정!
+        //값에 따라 이미지 변경!
+
+        mImg.invalidate(); //ondraw() 다시 호출하는 방법! (변경된 것으로 다시 이미지를 변경한다!)
+    }
+
+
 
 }
--- app/src/main/java/kr/hs/emirim/sbb9918/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/sbb9918/imagetest/MyImage.java
@@ -11,22 +11,43 @@ import android.view.View;
  */
 
 public class MyImage extends View{
+    int choose=MainActivity.ORIGINAL;//이미지 변경을 위한 변수 선언! 처음을 오리지널로 초기화!
+    Bitmap picture; //전역 변수로 선언하여 onDraw에서 picture 사용을 가능하게 함!
+
     MyImage(Context context){//문맥 설정 (현재 위치 : this)
         super(context);//view를 상속 받아서 사용 (view에는 default 생성자가 존재하지 않으므로 사용 하야함!
+        picture= BitmapFactory.decodeResource(getResources(),R.drawable.sizuku); //sizuku라는 이미지 파일을 bitmap파일로 변환하여 반환하는 역활 입니다!
+    }
+
+    public void setChoose(int choose) { //chooes의 값을 MainActivity에서 받아서 초기화!
+        this.choose = choose;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);//view클래스에 ondraw 오버라이딩
-        Bitmap picture= BitmapFactory.decodeResource(getResources(),R.drawable.sizuku); //sizuku라는 이미지 파일을 bitmap파일로 변환하여 반환하는 역활 입니다!
+
         float cx=getWidth()/0.2f; //view의 정 중앙 좌표!
         float cy=getWidth()/0.2f;
         float x=(getWidth()-picture.getWidth())*0.5f; //draw bitmap이 float형 이여서 float형이 좋음!
         float y=(getHeight()-picture.getHeight())*0.5f;  //정 중앙에 위치 하도록 함!
-        //canvas.rotate(45,cx,cy); 회전
-        //canvas.translate(-150,200); 이동
-        // canvas.scale(0.2f,0.3f,cx,cy); 크기 변경 0.2배
-        canvas.skew(0.4f,0.4f); //비틀기 세로 축을 기준으로!
+
+        switch (choose){
+            case MainActivity.ROTATE:
+                canvas.rotate(45,cx,cy);//회전
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200); //이동
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(0.2f,0.3f,cx,cy); //크기 변경 0.2배
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f,0.4f); //비틀기 세로 축을 기준으로!
+                break;
+        }
+
         canvas.drawBitmap(picture,x,y,null);
+       // picture.recycle();//system resource 반환!
     }
 }
